db1b971c5cade13380f6ac8c0bce6bfad9160054
YPandor <pandor.yunus@gmail.com>
2020-07-06 20:43:37 +0100

    initial commit

---
+++ resources/views/posts/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <form action="/p" enctype="multipart/form-data" method="POST">
+        @csrf
+        <div class="row">
+            <div class="col-8 offset-2">
+                <div class="ro">
+                    <h1>Add New Post</h1>
+                </div>
+                <div class="form-group row">
+                    <label for="caption" class="col-md-4 col-form-label">Post Caption</label>
+    
+                    
+                        <input id="caption" name="caption" type="text" class="form-control @error('caption') is-invalid @enderror" value="{{ old('caption') }}" required autocomplete="caption" autofocus>
+    
+                        @error('caption')
+                            <strong>{{ $message }}</strong>
+                        @enderror
+                    
+                </div>
+
+                <div class="row">
+                    <label for="image" class="col-md-4 col-form-label">Post Image</label>
+                    @error('image')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                     @enderror
+                    <input type="file" class="form-control-file" id="image" name="image">
+                </div>
+
+                <div class="row pt-4">
+                    <button class="btn btn-primary">Add New Post</button>
+                </div>
+
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
